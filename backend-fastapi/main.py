dd5e676b67592796d4a6335fd949f8916d13929d
Gaurav Sharan <91775940+gaurav8707@users.noreply.github.com>
2025-02-23 15:01:18 +0530

    fixed frontend route

--- backend-fastapi/main.py
+++ backend-fastapi/main.py
@@ -23,7 +23,7 @@ app = FastAPI(title="RAG Application")
 origins = [
     "http://localhost:3000",    # Next.js development server
     "http://127.0.0.1:3000",
-    "https://frontend-next-f551jnicm-gaurav-sharans-projects.vercel.app/",
+    "https://frontend-next-inky.vercel.app/",
 ]
 
 app.add_middleware(
