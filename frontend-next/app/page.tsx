f97b4728abd569944fe332efe09bf57b815c86a1
Gaurav Sharan <91775940+gaurav8707@users.noreply.github.com>
2025-02-23 16:58:57 +0530

    fixing page.tsx

--- frontend-next/app/page.tsx
+++ frontend-next/app/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_BASE_URL = process.env.API_BASE_URL;
 
 export default function Home() {
   const [url, setUrl] = useState("");
